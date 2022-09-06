b1f6d81cbb611d15afc8329c47d43bbc7e0d9798
umnicol <nicoltomms@gmail.com>
2022-09-06 13:16:13 +0200

    year

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
 let person ={
     firstname: 'Nicol',
     lastname: 'Hergelova',
-    favoriteSnack: 'WhiteKitKat'
+    favoriteSnack: 'WhiteKitKat',
+    year: 2022
 }
 
 console.log("My fav snack is " + person.favoriteSnack + " ,what is yours?")
