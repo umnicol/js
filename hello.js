5923c566f25e7073384c832537e31733e38e81bc
umnicol <nicoltomms@gmail.com>
2022-09-06 12:57:52 +0200

    added javascript

---
+++ hello.js
@@ -0,0 +1,7 @@
+let person ={
+    firstname: 'Nicol',
+    lastname: 'Hergelova',
+    favoriteSnack: 'WhiteKitKat'
+}
+
+console.log("My fav snack is " + person.favoriteSnack + " ,what is yours?")
